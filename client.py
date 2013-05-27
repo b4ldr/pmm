c84d7ef2cc053ea15889e2798712ff77509a75ba
John Bond <john.r.bond@gmail.com>
2013-05-27 23:37:40 +0200

    add simple client

---
+++ client.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import socket
+import json
+
+data = {'domains': [ { 'domain': 'pmm.johnbond.org'}, { 'domain': 'pmm2.jonbond.org'}] , 'data':'AABBCCDDEEFF'}
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(('192.168.1.5', 9999))
+s.send("blabablablabla")
+#s.send(json.dumps(data))
+result = json.loads(s.recv(1024))
+print result
+s.close()
